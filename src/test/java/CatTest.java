02944a427aa1f066f7d850acc762e51923ee6cac
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-04-02 13:59:54 +0400

    Add Cat tests

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,38 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    Cat cat;
+
+    @Mock
+    Feline feline;
+
+    @Before
+    public void createFeline (){
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void shouldBeMeow(){
+        String expected = "Мяу";
+        assertEquals(expected, cat.getSound());
+    }
+
+    @Test
+    public void shouldGetPredatorFood() throws Exception {
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(cat.getFood()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(expected, cat.getFood());
+    }
+}
