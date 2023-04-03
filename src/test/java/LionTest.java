b4d48e45832e56265152d539ad158e623475175f
IrinaBobarykina <ira.bobarykina5@gmail.com>
2023-04-03 21:14:01 +0400

    Fix using of mock in Lion test

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -32,16 +32,8 @@ public class LionTest {
     }
 
     @Test
-    public void getKittensReturnOne() throws Exception {
-        int expected = 1;
-        Mockito.when(lion.getKittens()).thenReturn(1);
-        assertEquals(expected, lion.getKittens());
-    }
-
-    @Test
-    public void getFoodReturnPredatorFood() throws Exception {
-        List<String> expected = List.of("Животные", "Птицы", "Рыба");
-        Mockito.when(lion.getFood()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        assertEquals(expected, lion.getFood());
+    public void getKittensCallFeline() {
+        lion.getKittens();
+        Mockito.verify(feline).getKittens();
     }
 }
